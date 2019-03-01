f2ac6f5b52242ce32863288a24249dc6228bc855
davemeneses <meneses.coffee@gmail.com>
2019-03-01 17:05:55 -0600

    added all the html and started on the js

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  var skateBrands = [
+    "welcome",
+    "independent",
+    "vans",
+    "primitive",
+    "thrasher",
+    "girl",
+    "transworld"
+  ];
+  var wrongGuesses; //letters guessed
+  var correctGuesses; //correctGuesses
+  var score; //times game won
+  var counter; //lives left
+  //random game words we are trying to guess
+  var wordSelect = "";
+  teamsList[Math.floor(Math.random() * teamsList.length)];
+  var letters = [];
+
+  //populate the empty array by splitting up the letters in the word
+  var letters = chosenWord.split("");
+};
