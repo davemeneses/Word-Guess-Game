beadb65ef687cad2ff2cb6c2c3d64bbcad15001e
davemeneses <meneses.coffee@gmail.com>
2019-03-03 22:18:57 -0600

    finished all I can bear to do tonight for the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
+// so #1 this was really hard. My tutor and I spent 2 hours working on this and she even made herself available to me after our session just through slack chat and I wasn't able to finish building the game.
+// I would like to think I'm more than half way there but I honestly have been working on this since Thursday and don't have the brain capacity to do any more tonight before it is due.
+// I have A LOT commented out at the bottom where I had built stuff, decided not to use it, and then stored it down there for later just in case. Pretty much anything past line 108 that's commented out you can ignore.
+
+// The things I was not able to do are: make the win counter go up, have a starting event function, display letters the user has guessed, have the game reset after each instance, make the page look at all presentable.
+
 // This top portion of code is just to define my variables that will be global and start creating the spots where players will guess letters.
 
 // I created a variable that is an array of all the skateboarding brands I chose to use
@@ -24,6 +30,7 @@ var remainingGuesses = 10;
 
 // }
 
+// funciton i built to select the word from my brands array
 function selectWord() {
   word = brands[Math.floor(Math.random() * brands.length)];
 }
@@ -44,14 +51,16 @@ function makeUnderscores() {
 }
 makeUnderscores();
 
-function displayWinsAndGuesses() {
+// this was the funciton i built to display my scores. Doesn't work perfect yet.
+function display() {
   document.getElementById("guessesRemaining").innerHTML =
     "Guesses Remaining: " + remainingGuesses;
   document.getElementById("wins").innerHTML = "You've won " + wins;
   document.getElementById("blanks").innerHTML = answerArray.join("");
 }
 
-displayWinsAndGuesses();
+display();
+
 document.onkeyup = function(event) {
   currentGuess = String.fromCharCode(event.keyCode).toLowerCase();
   console.log("User guess: " + currentGuess);
@@ -75,21 +84,28 @@ document.onkeyup = function(event) {
         }
       }
     }
-    // me trying to turn this into a string
-    // if (answerArray.toString() === brands[i]) {
-    //   won();
-    // }
   }
-  // just subtracts with each keypress
+  // just subtracts with each keypress. I was unable to make the if/else statement that if the letter didn't match any of the letters in the word that the guesses counter would not count down.
   if (remainingGuesses > 0) {
     remainingGuesses--;
+    document.getElementById("guessesRemaining").innerHTML =
+      "Guesses Remaining: " + remainingGuesses;
   } else if (remainingGuesses == 0) {
-    alert("you looooose, idiot");
+    alert("you looooose");
   }
 
   document.getElementById("blanks").innerHTML = answerArray.join("");
 
+  // console.log(remainingGuesses);
+  document.getElementById("blanks").innerHTML = answerArray;
+
+  // this section sort of worked. I was able to make all the users inputs a string and check that against my brands array and see if this string matched anything in there. I coudld display a win pop up but was unable to make my counter go up for wins.
   var remainingLetters = word.length;
+  if (word == answerArray.join("")) {
+    wins++;
+    display;
+    alert("you did it");
+  }
 };
 // getElementById example
 // document.getElementById("place").innerHTML = ("regular farts")
